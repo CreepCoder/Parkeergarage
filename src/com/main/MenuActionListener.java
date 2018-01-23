c732889438c893015c670411c5d8da4f1d2638a5
Lorenzooo <lorenzocg@hotmail.nl>
2018-01-23 13:54:25 +0100

    Menubar fixes

--- src/com/main/MenuActionListener.java
+++ src/com/main/MenuActionListener.java
@@ -25,30 +25,42 @@ public class MenuActionListener implements ActionListener {
 		if (e.getActionCommand().equals("Exit")) {
 			System.exit(0);
 		}
-		if (e.getActionCommand().equals("Piechart")) {
-			boxMessage = "View pie chart...";
+		if (e.getActionCommand().equals("Pie chart")) {
+			boxMessage = "Pie chart";
 			
 			if (Parkeergarage.viewpie.isShowing()) {
-				boxMessage += "WAS SHOWING";
+				boxMessage += " is now turned off.";
 				Parkeergarage.viewpie.setVisible(false);
+				
 			}
 			else {
-				boxMessage += "WAS NOT SHOWING";
+				boxMessage += " is now turned on.";
 				Parkeergarage.viewpie.setVisible(true);
 			}
 		}
 		if (e.getActionCommand().equals("Clock")) {
-			boxMessage = "View clock...";
+			boxMessage = "Clock";
 			
 			if (Parkeergarage.viewKlok.isShowing()) {
-				boxMessage += "WAS SHOWING";
+				boxMessage += " is now turned off.";
 				Parkeergarage.viewKlok.setVisible(false);
 			}
 			else {
-				boxMessage += "WAS NOT SHOWING";
+				boxMessage += " is now turned on.";
 				Parkeergarage.viewKlok.setVisible(true);
 			}
-
+		}
+		if (e.getActionCommand().equals("Carpark")) {
+			boxMessage = "Carpark";
+			
+			if (Parkeergarage.viewcarpark.isShowing()) {
+				boxMessage += " is now turned off.";
+				Parkeergarage.viewcarpark.setVisible(false);
+			}
+			else {
+				boxMessage += " is now turned on.";
+				Parkeergarage.viewcarpark.setVisible(true);
+			}
 		}
 		if (e.getActionCommand().equals("Default settings")) {
 			boxMessage = "Set default settings...";
--- src/com/main/Menubar.java
+++ src/com/main/Menubar.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -14,15 +16,19 @@ public class Menubar extends JMenuBar{
 	public Menubar(JFrame scherm)
 	{
 		super();
+		
 		// voeg menus toe aan de menubar
 		JMenu file = new JMenu("File");
 		JMenu view = new JMenu("View");
+		//JMenuItem view = new JMenuItem("View");
 		JMenu settings = new JMenu("Settings");
 		// voeg menu items toe aan de menus van de menubar
 		JMenuItem openFile = new JMenuItem("Open file");
 		JMenuItem exitFile = new JMenuItem("Exit");
-		JMenuItem viewPie = new JMenuItem("Piechart");
+		JMenuItem viewPie = new JMenuItem("Pie chart");
+		//JCheckBoxMenuItem viewPie = new JCheckBoxMenuItem("Pie chart");
 		JMenuItem viewClock = new JMenuItem("Clock");
+		JMenuItem viewCarPark = new JMenuItem("Carpark");
 		JMenuItem defaultSettings = new JMenuItem("Default settings");
 		
 		// Voeg de menus inclusief de menu items toe aan de menubar
@@ -33,6 +39,7 @@ public class Menubar extends JMenuBar{
 		file.add(exitFile);
 		view.add(viewPie);
 		view.add(viewClock);
+		view.add(viewCarPark);
 		settings.add(defaultSettings);
 		//test
 		// voeg de addActionListener toe aan de menu items
@@ -40,6 +47,7 @@ public class Menubar extends JMenuBar{
 		exitFile.addActionListener(new MenuActionListener());
 		viewPie.addActionListener(new MenuActionListener());
 		viewClock.addActionListener(new MenuActionListener());
+		viewCarPark.addActionListener(new MenuActionListener());
 		defaultSettings.addActionListener(new MenuActionListener());
 		
 		menubar.setVisible(true);
