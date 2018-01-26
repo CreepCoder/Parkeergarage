70db4a571ce03d550773e6af022f3d2a05f271bd
morc <m.clerks@st.hanze.nl>
2018-01-26 15:15:20 +0100

    histogram works now

--- src/com/mvc/view/ViewHistogram.java
+++ src/com/mvc/view/ViewHistogram.java
@@ -10,21 +10,25 @@ import com.mvc.model.Model;
 
 public class ViewHistogram extends AbstractView {
 	private static final long serialVersionUID = 5455934187803194147L;
-	JLabel label549 = new JLabel("540");
-	JLabel label410 = new JLabel("410");
-	JLabel label286 = new JLabel("286");
-	JLabel label162 = new JLabel("162");
-	JLabel label0 = new JLabel("0");
+	private JLabel label0 = new JLabel(""+ model.aantalLegeVakken);
+	private JLabel label1 = new JLabel(""+ model.aantalCarAdHoc);
+	private JLabel label2 = new JLabel(""+ model.aantalCarParkingPass);
+	private JLabel label3 = new JLabel(""+ model.aantalCarElektrisch);
+	private JLabel label4 = new JLabel(""+ model.aantalCarInvalide);
+	private JLabel label5 = new JLabel(""+ model.aantalCarMotor);
+	
 	
 	public ViewHistogram(Model model) {
 		super(model);
 		this.setSize(400, 230);
 		this.setLayout(null);
-		this.label(label0, 0, 219, 15, 10, true);
-		this.label(label162, 0, 155, 25, 10, true);
-		this.label(label286, 0, 105, 25, 10, true);
-		this.label(label410, 0, 55, 25, 10, true);
-		this.label(label549, 0, 5, 25, 10, true);
+		label(label0, 23, model.aantalLegeVakken, 30, 10, true);
+		label(label1, 99, model.aantalLegeVakken, 30, 10, true);
+		label(label2, 23, model.aantalLegeVakken, 30, 10, true);
+		label(label3, 23, model.aantalLegeVakken, 30, 10, true);
+		label(label4, 23, model.aantalLegeVakken, 30, 10, true);
+		label(label5, 23, model.aantalLegeVakken, 30, 10, true);
+		
 	}
 	
 
@@ -45,40 +49,63 @@ public class ViewHistogram extends AbstractView {
    
 		
 		g.setColor(Color.lightGray);
-		g.drawLine(0, 100, 399, 100);
-		g.drawLine(0, 50, 399, 50);
-		g.drawLine(0, 150, 399, 150);
-		g.drawLine(50, 229, 50, 0);
-		g.drawLine(100, 229, 100, 0);
-		g.drawLine(150, 229, 150, 0);
-		g.drawLine(200, 229, 200, 0);
-		g.drawLine(250, 229, 250, 0);
-		g.drawLine(300, 229, 300, 0);
-		g.drawLine(350, 229, 350, 0);
-		
+		g.drawLine(0, 46, 399, 46);
+		g.drawLine(0, 92, 399, 92);
+		g.drawLine(0, 138, 399, 138);
+		g.drawLine(0, 184, 399, 184);
+		g.drawLine(66, 229, 66, 0);
+		g.drawLine(133, 229, 133, 0);
+		g.drawLine(199, 229, 199, 0);
+		g.drawLine(265, 229, 265, 0);
+		g.drawLine(331, 229, 331, 0);
+
+	
+		int aantaladhoc= 228-model.aantalCarAdHoc/2;
+
 		int aantallege = 228-model.aantalLegeVakken/2;
 		
 		if (aantallege < 2) {
 			aantallege = 1;
 		}
-		int aantaladhoc= 228-model.aantalCarAdHoc/2;
 		
 		if (aantaladhoc < 2) {
 			aantaladhoc = 1;
 		}
 
-		createBar(g, ColorList.LEGE_VAKKEN, 33, aantallege, 50, 229);
-		createBar(g, ColorList.CAR_AD_HOC, 96, aantaladhoc, 50, 229);
-		createBar(g, ColorList.CAR_PARKING_PASS, 165, 228-model.aantalCarParkingPass/2 , 50, 229);
-		createBar(g, ColorList.ELEKTRISCHE_CAR, 231,  228-model.aantalCarElektrisch/2, 50,229);
-		createBar(g, ColorList.INVALIDE_CAR, 297,  228-model.aantalCarInvalide/2, 50, 229);
-		createBar(g, ColorList.MOTOR, 363, 228-model.aantalCarMotor/2, 50, 229);
+		createBar(g, ColorList.LEGE_VAKKEN, 10 , aantallege, 46 , 229);
+		createBar(g, ColorList.CAR_AD_HOC, 76, aantaladhoc, 46, 229);
+		createBar(g, ColorList.CAR_PARKING_PASS, 142, 228-model.aantalCarParkingPass/2 , 46, 229);
+		createBar(g, ColorList.ELEKTRISCHE_CAR, 208,  228-model.aantalCarElektrisch/2, 46,229);
+		createBar(g, ColorList.INVALIDE_CAR, 274,  228-model.aantalCarInvalide/2, 46, 229);
+		createBar(g, ColorList.MOTOR, 340, 228-model.aantalCarMotor/2, 46, 229);
+		label0.setText(""+ model.aantalLegeVakken);
+		label1.setText(""+ model.aantalCarAdHoc);
+		label2.setText(""+ model.aantalCarParkingPass);
+		label3.setText(""+ model.aantalCarElektrisch);
+		label4.setText(""+ model.aantalCarElektrisch);
+		label5.setText(""+ model.aantalCarMotor);
+		labels();
 	}
 	
 	private void createBar(Graphics g, Color color, int x, int y, int width, int height) {
 		g.setColor(color);
 		g.fill3DRect(x, y, width, height, true);
 		 	}
+	
+	private void labels() {
+		int aantallege = 218-model.aantalLegeVakken/2;
+		if (aantallege < -1) {
+			label0.setLocation(23, 208-model.aantalCarAdHoc/2);
+		}else {
+		label0.setLocation(23, aantallege);
+		}
+		System.out.println(aantallege);
+		label1.setLocation(99, 218-model.aantalCarAdHoc/2);
+		label2.setLocation(165, 218-model.aantalCarParkingPass/2);
+		label3.setLocation(231, 218-model.aantalCarElektrisch/2);
+		label4.setLocation(297, 218-model.aantalCarInvalide/2);
+		label5.setLocation(363, 218-model.aantalCarMotor/2);
+	}
 
 	public void label(JLabel label, int x, int y, int width, int height, boolean visible) {
 		label.setLocation(x, y);
