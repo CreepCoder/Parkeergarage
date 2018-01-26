379b3709598e30fdfa125cec9af379d574522fff
morc <m.clerks@st.hanze.nl>
2018-01-26 09:30:19 +0100

    histogram werkt nu 100%

--- src/com/mvc/view/ViewHistogram.java
+++ src/com/mvc/view/ViewHistogram.java
@@ -11,7 +11,7 @@ public class ViewHistogram extends AbstractView {
 
 	public ViewHistogram(Model model) {
 		super(model);
-		this.setSize(400, 200);
+		this.setSize(400, 230);
 		this.setLayout(null);
 	}
 	
@@ -22,7 +22,7 @@ public class ViewHistogram extends AbstractView {
 	public void paintComponent(Graphics g) {
 		// Creëer achtergrond
 		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, 400, 199);
+		g.fillRect(0, 0, 400, 229);
 
 		
 		// Creëer belijning
@@ -31,20 +31,30 @@ public class ViewHistogram extends AbstractView {
 		g.drawLine(0, 0, 0, 200);
 		g.drawLine(0, 199, 399, 199);
 		g.drawLine(399, 0, 399, 199);
-    
+   
+		int aantallege = 199-model.aantalLegeVakken/2;
 		
-		createBar(g, ColorList.LEGE_VAKKEN, 3, 199-model.aantalLegeVakken/2, 60, 199);
-		createBar(g, ColorList.CAR_AD_HOC, 66, 199-model.aantalCarAdHoc/2, 60, 199);
-		createBar(g, ColorList.CAR_PARKING_PASS, 135, 199-model.aantalCarParkingPass/2, 60, 199);
+		if (aantallege < 2) {
+			aantallege = 1;
+		}
+		int aantaladhoc= 199-model.aantalCarAdHoc/2;
+		
+		if (aantaladhoc < 2) {
+			aantaladhoc = 1;
+		}
+
+		createBar(g, ColorList.LEGE_VAKKEN, 3, aantallege, 60, 199);
+		createBar(g, ColorList.CAR_AD_HOC, 66, aantaladhoc, 60, 199);
+		createBar(g, ColorList.CAR_PARKING_PASS, 135, 199-model.aantalCarParkingPass/2 , 60, 199);
 		createBar(g, ColorList.ELEKTRISCHE_CAR, 201, 1, 60, model.aantalCarElektrisch/2);
 		createBar(g, ColorList.INVALIDE_CAR, 267, 1, 60, model.aantalCarInvalide/2);
 		createBar(g, ColorList.MOTOR, 333, 1, 60, model.aantalCarMotor/2);
-		System.out.println(199-model.aantalLegeVakken/2);
 	}
 	
 	private void createBar(Graphics g, Color color, int x, int y, int width, int height) {
 		g.setColor(color);
 		g.fillRect(x, y, width, height);
 		 	}
+
 	
 }
